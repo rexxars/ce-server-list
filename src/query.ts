ee10322052823965e26966d7d5472149c75bef37
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-02-24 17:14:11 -0800

    feat: remove udp logging

--- src/query.ts
+++ src/query.ts
@@ -1,5 +1,3 @@
-import fs from 'fs'
-import path from 'path'
 import dgram from 'dgram'
 import LruCache from 'lru-cache'
 
@@ -17,7 +15,6 @@ import {
 const SLASH = '\\'.charCodeAt(0)
 const FIFTEEN_DAYS = 1000 * 60 * 60 * 24 * 15
 const geoIpCache = new LruCache<string, string | false>({max: 500, maxAge: FIFTEEN_DAYS})
-const udpLog = fs.createWriteStream(path.join(__dirname, '..', 'data', 'udp.log'))
 
 export async function queryServer(ip: string, port: number): Promise<Server> {
   const chunker = waitForQueryResponses(2)
@@ -25,9 +22,6 @@ export async function queryServer(ip: string, port: number): Promise<Server> {
   const socket = dgram.createSocket('udp4')
   const {promise: socketConnect, reject} = getRejectable<QueryResponse[]>()
   socket.on('message', (msg) => {
-    udpLog.write(`[${new Date().toISOString()}] `)
-    udpLog.write(msg)
-    udpLog.write('\n')
 
     if (msg[0] !== SLASH) {
       log.warn('Got unknown response type - not slash-prefixed. Skipped.')
@@ -126,7 +120,6 @@ export function waitForQueryResponses(
 }
 
 export function closeQueries(): void {
-  udpLog.close()
 }
 
 function toKeyValue(msg: Buffer): Record<string, string> {
