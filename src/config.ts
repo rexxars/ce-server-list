534aff5b8e78dbffba80acabcc6f2ca75ca2ce21
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-06-15 21:20:56 -0700

    feat: read token from cli config if not provided

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+import {join} from 'node:path'
+import {homedir} from 'node:os'
+import {readFile} from 'node:fs/promises'
+
 import type {Config} from './typings.ts'
 
 export const config: Config = {
@@ -5,6 +9,14 @@ export const config: Config = {
   host: process.env.CE_SERVER_LIST_HOST || '0.0.0.0',
   logLevel: process.env.CE_SERVER_LIST_LOG_LEVEL || 'info',
   checkThresholdMs: parseInt(process.env.CE_SERVER_LIST_CHECK_THRESHOLD || '', 10) || 30000,
-  sanityToken: process.env.CE_SERVER_LIST_SANITY_TOKEN || '',
+  sanityToken: process.env.CE_SERVER_LIST_SANITY_TOKEN || (await tryReadSanityConfigToken()) || '',
   httpPort: parseInt(process.env.CE_SERVER_LIST_HTTP_PORT || '', 10) || 8080,
+}
+
+function tryReadSanityConfigToken() {
+  const configPath = join(homedir(), '.config', 'sanity', 'config.json')
+  return readFile(configPath, 'utf-8')
+    .then((data) => JSON.parse(data))
+    .catch(() => ({}))
+    .then((config) => config.authToken)
 }
